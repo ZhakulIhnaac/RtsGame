74bb6cf2d7f3f3f01585171dfa88bbe563396647
İbrahim Mehmet Akçal <mibrahimakcal@gmail.com>
2020-07-07 19:54:04 +0300

    Menü temelleri atıldı.

---
+++ Assets/Scripts/environment-scripts/Menu/GameMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    GameObject currentActivePage;
+    GameObject mainMenu;
+    GameObject loadMenu;
+    GameObject optionsMenu;
+
+    private void Start()
+    {
+        mainMenu = GameObject.Find("MainMenu");
+        loadMenu = GameObject.Find("LoadMenu");
+        optionsMenu = GameObject.Find("OptionsMenu");
+        loadMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        currentActivePage = mainMenu;
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("NewGame", LoadSceneMode.Single);
+    }
+
+    public void changeMenu(GameObject menuToLoad)
+    {
+        currentActivePage.SetActive(false);
+        currentActivePage = menuToLoad;
+        currentActivePage.SetActive(true);
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene("NewGame", LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+
+    }
+
+}
